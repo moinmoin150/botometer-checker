a08683c0b59bd095716d458b1d70938dcefb1d62
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-09-22 17:21:51 -0500

    Update botometer_checker.py

--- botometer_checker.py
+++ botometer_checker.py
@@ -2,6 +2,10 @@ import streamlit as st
 import botometer as boto
 import pandas as pd
 
+@st.cache
+def convert_df_to_csv(df):
+  return df.to_csv().encode('utf-8-sig')
+
 rapidapi_key = st.secrets["rapidapi_key"]
 twitter_app_auth = {
     'consumer_key': st.secrets["consumer_key"],
@@ -15,7 +19,8 @@ bom = boto.Botometer(wait_on_ratelimit=True,
 
 st.markdown("# Botometer Checker")
 uploaded_file = st.file_uploader("Please upload a CSV spreadsheet with usernames or user IDs in a column (either with or without @ is okay)", type=["csv"])
-col = st.text_input('Enter the name of the column with usernames or user iDs')
+col = st.text_input('Enter the name of the column with usernames or user iDs (case sensitive)')
+st.write("* press enter and wait... *")
 
 if (uploaded_file is not None) and (len(col)>0):
     df = pd.read_csv(uploaded_file)
@@ -30,14 +35,14 @@ if (uploaded_file is not None) and (len(col)>0):
             result_l.append(result['cap']['universal'])
         except:
             result_l.append('error')
-        if len(result_l) % 100 == 0:
+        if len(result_l) % 10 == 0:
             st.write('successfully checked', len(result_l), 'accounts')
 
     df['botometer_result'] = result_l
 
     st.download_button(
         label="Download data as CSV",
-        data=df,
+        data=convert_df_to_csv(df),
         file_name=f'{uploaded_file.name}_bot_checked.csv',
         mime='text/csv',
     )
